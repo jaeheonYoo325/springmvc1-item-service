17a9add40564268b9960f804afb78e4b6bc4dfb2
jaeheonYoo325 <yoojh0325@gmail.com>
2022-07-03 19:48:46 +0900

    2022.07.03 : item-service 프로젝트 생성 및 상품 도메인 개발.

---
+++ src/main/java/com/springmvc1/springmvc1itemservice/Springmvc1ItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc1.springmvc1itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc1ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvc1ItemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvc1/springmvc1itemservice/Springmvc1ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc1.springmvc1itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc1ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
